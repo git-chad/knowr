5b816d2e9c95f678db1623d9b86342a6cafb7ff9
git-chad <tobimocc@gmail.com>
2024-03-09 12:05:58 -0300

    fix(dev): wrapped login in suspense boundary

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,13 +1,13 @@
-'use client'
+"use client";
+import { Suspense } from "react";
 import Container from "@/components/ui/container";
-import { KeySquare } from "lucide-react";
-import { Github } from "lucide-react";
+import { KeySquare, Github } from "lucide-react";
 import Image from "next/image";
 import googleIcon from "@/public/icons/google-icon.svg";
 import createSupabaseClient from "@/lib/supabase/client";
 import { useSearchParams } from "next/navigation";
 
-const AuthPage = () => {
+const AuthPageContent = () => {
   const params = useSearchParams();
   const next = params.get("next");
 
@@ -16,37 +16,46 @@ const AuthPage = () => {
     supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: `${location.origin}/auth/callback?next=${encodeURIComponent(
-          next || "/"
-        )}`,
+        redirectTo: `${
+          window.location.origin
+        }/auth/callback?next=${encodeURIComponent(next || "/")}`,
       },
     });
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-between mesh text-neutral-100">
-      <Container className="py-32 flex flex-col">
-        <div className="w-96 rounded-md border backdrop-blur-2xl bg-neutral-950/20 border-neutral-700 p-5 flex flex-col">
-          <div className="flex items-center justify-between">
-            <span className="text-lg font-bold">Sign in (or up) to Knowr</span>
-            <KeySquare />
-          </div>
-          <span className="text-neutral-400">and start contributing</span>
-          <button
-            onClick={() => handleOauthLogin("github")}
-            className="border border-neutral-700 flex items-center justify-center px-5 py-1 rounded-md text-sm mt-8"
-          >
-            <Github className="w-6 h-6 mr-2" />
-            Join via Github
-          </button>
-          <button
-            onClick={() => handleOauthLogin("google")}
-            className="border border-neutral-700 flex items-center justify-center px-5 py-1 rounded-md text-sm mt-2"
-          >
-            <Image src={googleIcon} className="w-5 h-5 mr-2" /> Join via Google
-          </button>
+    <Container className="py-32 flex flex-col">
+      <div className="w-96 rounded-md border backdrop-blur-2xl bg-neutral-950/20 border-neutral-700 p-5 flex flex-col">
+        <div className="flex items-center justify-between">
+          <span className="text-lg font-bold">Sign in (or up) to Knowr</span>
+          <KeySquare />
         </div>
-      </Container>
+        <span className="text-neutral-400">and start contributing</span>
+        <button
+          onClick={() => handleOauthLogin("github")}
+          className="border border-neutral-700 flex items-center justify-center px-5 py-1 rounded-md text-sm mt-8"
+        >
+          <Github className="w-6 h-6 mr-2" />
+          Join via Github
+        </button>
+        <button
+          onClick={() => handleOauthLogin("google")}
+          className="border border-neutral-700 flex items-center justify-center px-5 py-1 rounded-md text-sm mt-2"
+        >
+          <Image src={googleIcon} alt="Google Icon" className="w-5 h-5 mr-2" />{" "}
+          Join via Google
+        </button>
+      </div>
+    </Container>
+  );
+};
+
+const AuthPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-between mesh text-neutral-100">
+      <Suspense fallback={<div>Loading...</div>}>
+        <AuthPageContent />
+      </Suspense>
     </div>
   );
 };
--- middleware.js
+++ middleware.js
@@ -11,7 +11,7 @@ export async function middleware(request) {
   });
 
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
   const supabase = createServerClient(supabaseUrl, supabaseKey, {
     cookies: {
