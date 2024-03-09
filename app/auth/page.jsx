f293473d58b2732e3ba308fdae3b1c7ee9ff9463
git-chad <tobimocc@gmail.com>
2024-03-09 11:43:39 -0300

    ...

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,13 +1,52 @@
-import LoginForm from "@/components/signin/login-form";
+'use client'
 import Container from "@/components/ui/container";
-import React from "react";
+import { KeySquare } from "lucide-react";
+import { Github } from "lucide-react";
+import Image from "next/image";
+import googleIcon from "@/public/icons/google-icon.svg";
+import createSupabaseClient from "@/lib/supabase/client";
+import { useSearchParams } from "next/navigation";
 
 const AuthPage = () => {
+  const params = useSearchParams();
+  const next = params.get("next");
+
+  const handleOauthLogin = (provider) => {
+    const supabase = createSupabaseClient();
+    supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        redirectTo: `${location.origin}/auth/callback?next=${encodeURIComponent(
+          next || "/"
+        )}`,
+      },
+    });
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-between mesh text-neutral-100">
-        <Container className='py-32 flex flex-col'>
-            <LoginForm/>
-        </Container>
+      <Container className="py-32 flex flex-col">
+        <div className="w-96 rounded-md border backdrop-blur-2xl bg-neutral-950/20 border-neutral-700 p-5 flex flex-col">
+          <div className="flex items-center justify-between">
+            <span className="text-lg font-bold">Sign in (or up) to Knowr</span>
+            <KeySquare />
+          </div>
+          <span className="text-neutral-400">and start contributing</span>
+          <button
+            onClick={() => handleOauthLogin("github")}
+            className="border border-neutral-700 flex items-center justify-center px-5 py-1 rounded-md text-sm mt-8"
+          >
+            <Github className="w-6 h-6 mr-2" />
+            Join via Github
+          </button>
+          <button
+            onClick={() => handleOauthLogin("google")}
+            className="border border-neutral-700 flex items-center justify-center px-5 py-1 rounded-md text-sm mt-2"
+          >
+            <Image src={googleIcon} className="w-5 h-5 mr-2" /> Join via Google
+          </button>
+        </div>
+      </Container>
     </div>
   );
 };
